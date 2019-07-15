6802215a6f38beb43739f504bf749cb095e3b014
glazey132 <alglaze132@gmail.com>
2019-07-14 23:09:13 -0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const Url = require('../models/Url');
+
+// @route      GET /:code
+// @desc       Redirect to long/original URL
+router.get('/:code', async (req, res) => {
+    try {
+        const url = await Url.findOne({ urlCode: req.params.code });
+        console.log('req.params => ', req.params)
+
+        if (url) {
+            res.redirect(url.longUrl);
+        } else {
+            res.status(404).json('No url found')
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json('Server error');
+    }
+})
+
+module.exports = router;
